74fb2ebd9c99a51a32413751d7970c4b739d3203
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-04-23 09:49:50 -0600

    deploy test

---
+++ app/deploy/deploy.ci.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+cp .env.ci .env
+/opt/cpanel/ea-php74/root/bin/php /opt/cpanel/composer/bin/composer install
+/opt/cpanel/ea-php74/root/bin/php artisan down
+/opt/cpanel/ea-php74/root/bin/php artisan migrate:fresh --seed -v
+/opt/cpanel/ea-php74/root/bin/php artisan storage:link
+/opt/cpanel/ea-php74/root/bin/php artisan config:clear
+/opt/cpanel/ea-php74/root/bin/php artisan cache:clear
+/opt/cpanel/ea-php74/root/bin/php artisan route:clear
+/opt/cpanel/ea-php74/root/bin/php artisan view:clear
+/opt/cpanel/ea-php74/root/bin/php artisan up
