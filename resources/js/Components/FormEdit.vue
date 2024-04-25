45c794eea2a74258f7a5d0baf1e317948c9abb9b
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-04-25 17:50:20 -0600

    get label fix

--- resources/js/Components/FormEdit.vue
+++ resources/js/Components/FormEdit.vue
@@ -1,33 +1,39 @@
 <template>
-    <form @submit.prevent="handleSubmit()" class="bg-white shadow-md rounded px-8 pt-6 pb-8">
-        <div class="mt-2" v-for="field in fields">
-            <label class="block text-gray-700 text-sm font-bold mb-2">
-                {{ field.label }}
-            </label>
-            
-            <TextInput v-if="field.type === 'varchar' || field.type === 'longtext'" v-model="formData[field.slug]"/>
+    <template v-if="fields.length">
+        <form @submit.prevent="handleSubmit()" class="bg-white shadow-md rounded px-8 pt-6 pb-8">
+            <div class="mt-2" v-for="field in fields" k>
+                <label class="block text-gray-700 text-sm font-bold mb-2">
+                    {{ showLabel(field.slug) }}
+                </label>
+                
+                <TextInput v-if="field.type === 'varchar' || field.type === 'longtext'" v-model="formData[field.slug]"/>
 
-            <FileInput v-if="field.type === 'file' " v-on:file-selected="handleFileSelected"/>
-            
-            <NumberInput v-else-if="field.type === 'decimal'"  v-model="formData[field.slug]"/>
-            
-            <Select v-else-if="field.type === 'select'"  v-model="formData[field.slug]" :options="field.options" :default="formData[field.slug]" />
-            <div class="error" v-if="errors[field.slug]">{{strings.required}}</div>
+                <FileInput v-if="field.type === 'file' " v-on:file-selected="handleFileSelected"/>
+                
+                <NumberInput v-else-if="field.type === 'decimal'"  v-model="formData[field.slug]"/>
+                
+                <Select v-else-if="field.type === 'select'"  v-model="formData[field.slug]" :options="field.options" :default="formData[field.slug]" />
+                <div class="error" v-if="errors[field.slug]">{{strings.required}}</div>
+            </div>
+
+        <div class="mt-5">
+                <PrimaryButton class="mx-2">
+                    Guardar
+                </PrimaryButton>
+                <SecondaryButton @click="emit('close')">
+                    Cancelar
+                </SecondaryButton>
         </div>
-
-       <div class="mt-5">
-            <PrimaryButton class="mx-2">
-                Guardar
-            </PrimaryButton>
-            <SecondaryButton @click="emit('close')">
-                Cancelar
-            </SecondaryButton>
-       </div>
-    </form>
+        </form>
+    </template>
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8" v-else-if="!loader">
+        <p class="text-xl">No hay campos que mostrar</p>
+    </div>
 </template>
 <script setup>
     import TextInput from '@/Components/TextInput.vue';
     import NumberInput from '@/Components/NumberInput.vue';
+    import FileInput from '@/Components/FileInput.vue';
     import Select from '@/Components/Select.vue';
     import PrimaryButton from '@/Components/PrimaryButton.vue';
     import SecondaryButton from '@/Components/SecondaryButton.vue';
@@ -35,6 +41,7 @@
     import {ref,onMounted,defineEmits} from 'vue';
     import axios from 'axios';
     import strings from '@/utils/strings.js'
+    import showLabel from '@/helpers/showLabel';
     
     const emit  = defineEmits(['close']);
 
@@ -53,6 +60,7 @@
     const _token = window.csrf_token;
     const app_url = window.app_url;
     const errors = ref([]);
+    const loader = ref(false);
    
 
     const fields = ref([]);
@@ -64,10 +72,11 @@
 
     onMounted(async()=>{
         try {
-            
+            loader.value = true;
             const response = await axios(`${app_url}/${props.route}/${props.editId}/edit`);
+            loader.value = false;
             fields.value = response.data.fields;
-            console.log(response.data);
+
             clearFormData();
             fillInfo(response.data.item);
     
--- resources/js/helpers/showLabel.js
+++ resources/js/helpers/showLabel.js
@@ -2,6 +2,7 @@ import strings from '@/utils/strings'
 
 
 const showLabel = (label)=>{
+    console.log(label,typeof label);
     if (label && label.startsWith("https")) {
         
         return `<a href="${label}" target="_blank"><i class="fa-solid fa-image"></i></a>`;
