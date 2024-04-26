c9e539430af17db76fe26045793a06bf630be1b8
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-04-26 17:37:39 -0600

    clients clean a working new system

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,14 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Client;
 use Illuminate\Http\Request;
 use App\Services\ClientService;
+use App\Services\ValidateDataService;
 use Inertia\Inertia;
 use App\Utils\Utils;
 
 class ClientController extends Controller
 {
+    protected $service;
+    protected $validate;
+
     public function __construct(ClientService $clientService)
     {
         $this->service = $clientService;
+        $this->rules = [
+            'name' => 'required|string|max:255',
+            'contact_name' => 'required|string|max:255',
+            'phone' => 'required|string',
+            'address' => 'required|string|max:255',
+            'email' => 'required|email',
+        ];
     }
     /**
      * Display a listing of the resource.
@@ -30,7 +41,7 @@ class ClientController extends Controller
      */
     public function create()
     {
-        $fields = Utils::getFields('clients');
+        $fields = $this->service->create();
         return response()->json($fields);
     }
 
@@ -39,7 +50,15 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        return  $this->service->create($request);
+        
+        $validatedData = new ValidateDataService($request->all(), $this->rules);
+        $validatedData = $validatedData->getValidatedData();
+
+        if($validatedData['status']){
+            return $item = $this->service->store($validatedData['data']);    
+        }else{
+            return response()->json(['errors'=>$validatedData['errors']], 422);
+        } 
     }
 
     /**
@@ -57,17 +76,26 @@ class ClientController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Client $client)
+    public function edit($id)
     {
-        //
+        $fields = $this->service->edit($id);
+        return response()->json($fields);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Client $client)
+    public function update(Request $request, $id)
     {
-        //
+        $validatedData = new ValidateDataService($request->all(), $this->rules);
+        $validatedData = $validatedData->getValidatedData();
+
+        if($validatedData['status']){
+            $item = $this->service->update($id,$validatedData['data']);    
+        }else{
+            return response()->json(['errors'=>$validatedData['errors']], 422);
+        } 
+        
     }
 
     /**
@@ -75,7 +103,8 @@ class ClientController extends Controller
      */
     public function destroy($id)
     {
-        return $this->service->delete($id);
+        $this->service->delete($id);
+        return Inertia::location(route('clientes.index'));
         
     }
 
