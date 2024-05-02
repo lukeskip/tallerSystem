652e6edb5083464a964ae13330b7f53ba636264c
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-05-02 08:57:25 -0600

    fix

--- resources/views/mails/default/mail/html/header.blade.php
+++ resources/views/mails/default/mail/html/header.blade.php
@@ -2,9 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ asset('img/logo.png') }}" class="logo" alt="Laravel Logo">
-@else
+<img src="{{ asset('img/logo.png') }}" class="logo" alt="Taller 1100">
+
 {{ $slot }}
 @endif
 </a>
