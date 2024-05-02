e7936e37505a4411e5122d827cccd6b9ee17c0d3
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-05-02 10:25:19 -0600

    tenmplate mail fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,10 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+    <img src="{{asset('img/logo.png')}}" class="logo" alt="Laravel Logo">
+
+    {{ $slot }}
+
 </a>
 </td>
 </tr>
