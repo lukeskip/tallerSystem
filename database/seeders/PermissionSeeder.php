aae9a1855a842d2b966f3d6c8f6be938229bd660
Cheko Garcia <contacto@chekogarcia.com.mx>
2026-07-14 07:23:42 -0600

    refactor: use firstOrCreate in PermissionSeeder to prevent duplicate entries during seeding

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -13,66 +13,66 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        Permission::create(['name' => 'create user','guard_name' => 'web']);
-        Permission::create(['name' => 'read user','guard_name' => 'web']);
-        Permission::create(['name' => 'edit user','guard_name' => 'web']);
-        Permission::create(['name' => 'delete user','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create user','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read user','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit user','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete user','guard_name' => 'web']);
 
-        Permission::create(['name' => 'create client','guard_name' => 'web']);
-        Permission::create(['name' => 'read client','guard_name' => 'web']);
-        Permission::create(['name' => 'edit client','guard_name' => 'web']);
-        Permission::create(['name' => 'delete client','guard_name' => 'web']);
-
-        
-        Permission::create(['name' => 'create project','guard_name' => 'web']);
-        Permission::create(['name' => 'read project','guard_name' => 'web']);
-        Permission::create(['name' => 'edit project','guard_name' => 'web']);
-        Permission::create(['name' => 'delete project','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create client','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read client','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit client','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete client','guard_name' => 'web']);
 
         
-        Permission::create(['name' => 'create invoice','guard_name' => 'web']);
-        Permission::create(['name' => 'read invoice','guard_name' => 'web']);
-        Permission::create(['name' => 'edit invoice','guard_name' => 'web']);
-        Permission::create(['name' => 'delete invoice','guard_name' => 'web']);
-
-        Permission::create(['name' => 'create note','guard_name' => 'web']);
-        Permission::create(['name' => 'read note','guard_name' => 'web']);
-        Permission::create(['name' => 'edit note','guard_name' => 'web']);
-        Permission::create(['name' => 'delete note','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create project','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read project','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit project','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete project','guard_name' => 'web']);
 
         
-        Permission::create(['name' => 'create invoice_item','guard_name' => 'web']);
-        Permission::create(['name' => 'read invoice_item','guard_name' => 'web']);
-        Permission::create(['name' => 'edit invoice_item','guard_name' => 'web']);
-        Permission::create(['name' => 'delete invoice_item','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create invoice','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read invoice','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit invoice','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete invoice','guard_name' => 'web']);
+
+        Permission::firstOrCreate(['name' => 'create note','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read note','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit note','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete note','guard_name' => 'web']);
 
         
-        Permission::create(['name' => 'create income','guard_name' => 'web']);
-        Permission::create(['name' => 'read income','guard_name' => 'web']);
-        Permission::create(['name' => 'edit income','guard_name' => 'web']);
-        Permission::create(['name' => 'delete income','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create invoice_item','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read invoice_item','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit invoice_item','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete invoice_item','guard_name' => 'web']);
 
         
-        Permission::create(['name' => 'create outcome','guard_name' => 'web']);
-        Permission::create(['name' => 'read outcome','guard_name' => 'web']);
-        Permission::create(['name' => 'edit outcome','guard_name' => 'web']);
-        Permission::create(['name' => 'delete outcome','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create income','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read income','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit income','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete income','guard_name' => 'web']);
 
         
-        Permission::create(['name' => 'create provider','guard_name' => 'web']);
-        Permission::create(['name' => 'read provider','guard_name' => 'web']);
-        Permission::create(['name' => 'edit provider','guard_name' => 'web']);
-        Permission::create(['name' => 'delete provider','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create outcome','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read outcome','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit outcome','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete outcome','guard_name' => 'web']);
 
-        Permission::create(['name' => 'create category','guard_name' => 'web']);
-        Permission::create(['name' => 'read category','guard_name' => 'web']);
-        Permission::create(['name' => 'edit category','guard_name' => 'web']);
-        Permission::create(['name' => 'delete category','guard_name' => 'web']);
+        
+        Permission::firstOrCreate(['name' => 'create provider','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read provider','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit provider','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete provider','guard_name' => 'web']);
 
-        Permission::create(['name' => 'create fabric','guard_name' => 'web']);
-        Permission::create(['name' => 'read fabric','guard_name' => 'web']);
-        Permission::create(['name' => 'edit fabric','guard_name' => 'web']);
-        Permission::create(['name' => 'delete fabric','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'create category','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read category','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit category','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete category','guard_name' => 'web']);
+
+        Permission::firstOrCreate(['name' => 'create fabric','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'read fabric','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit fabric','guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete fabric','guard_name' => 'web']);
         
     }
 }
